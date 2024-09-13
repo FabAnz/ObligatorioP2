fa3ff2950b4e6c746800c7f4b925e76045d9eaf8
FabAnz <fabian.methil@gmail.com>
2024-09-13 17:56:07 -0300

    Finalizado, arreglar listas v1

--- LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
+++ LogicaNegocio/obj/Debug/net8.0/LogicaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271093d85f6840e41fce3dd936d9568c0cea6c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5841414cfd82a2fbb918f6aff7102426d53168b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
